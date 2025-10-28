541c7698bdf514a86e98d1e4fe3d91d6d37d1388
Hassan314159265 <hassan.ramos@gmail.com>
2025-10-28 15:32:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// In the future, you can use fetch() here to get real data from your ESP32 or an API
+document.getElementById('device-data').textContent = "Example: Temperature 22°C, Humidity 45%";
